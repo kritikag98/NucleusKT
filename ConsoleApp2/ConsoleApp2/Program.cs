d9d3288ddee225dc57fe9b210bf2e94c31e5ebeb
kritikag98 <kritika.g@homelane.com>
2022-08-08 10:45:08 +0530

    Basic C# application

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double sum = a + b;
+            double diff = a - b;
+            double prd = a * b;
+            Console.WriteLine("Sum is "+sum);
+            Console.WriteLine("Difference is: " + diff);
+            Console.WriteLine("Product is: " + prd);
+            Console.ReadLine();
+        }
+    }
+}
