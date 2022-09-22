f283af52eb7ebcf74fee467680f4ca4b12f485c7
kritikag98 <kritika.g@homelane.com>
2022-09-22 14:20:45 +0530

    The final code is done.

---
+++ Code/Code/DBConnection.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Code;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+
+namespace ConsoleApp3
+{
+
+    internal class DBConnection
+    {
+        //List for adding the changes made in a file
+        List<Changes> list = new List<Changes>();
+
+        //Path to the report
+        public string path = @"C:\Users\kritika.g\Documents\Report.csv";
+
+        //List of column values to be ignored while adding changes to the report
+        static string[] ignoredVals = {"ModifiedById", "ModifiedDate", "CreatedDate", "CreatedById", "CreatedByID", "ModifiedByID"};
+        List<string> ignoredColumns = new List<string>(ignoredVals);
+
+
+        //Variables for establishing a connection to the connection string
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string userId;
+        private string password;
+        private string connectionString;
+
+
+        //Establish a connection string for the publish database
+        private string PublishString()
+        {
+            server = "localhost";
+            database = "nucleus_config_homelane_sandbox_published";
+            userId = "root";
+            password = "homelane@123";
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + userId + ";" + "PASSWORD=" + password + ";";
+            return connectionString;
+        }
+
+        //Establishing a connection for the live database
+        private string LiveString()
+        {
+            server = "localhost";
+            database = "nucleus_config_homelane_sandbox_unpublished";
+            userId = "root";
+            password = "homelane@123";
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + userId + ";" + "PASSWORD=" + password + ";";
+            return connectionString;
+        }
+
+        //Function for reading a datatable for a given query
+        public DataTable Read(string sSQLQuery, string connection)
+        {
+
+            DataTable dtResult = new DataTable();
+
+            using (MySqlConnection myConnection = new MySqlConnection(connection))
+            {
+                using (MySqlCommand myCommand = new MySqlCommand(sSQLQuery, myConnection))
+                {
+                    using (MySqlDataAdapter myAdapter = new MySqlDataAdapter(myCommand))
+                    {
+                        myConnection.Open();
+                        myAdapter.Fill(dtResult);
+                    }
+                }
+            }
+
+            return dtResult;
+        }
+        
+        //Function for getting the last publish date
+        public string LastPublishDate()
+        {
+            string query = "select PublishDate from publish ORDER BY PublishDate DESC LIMIT 1";
+            DataTable lastPublishDt = Read(query, PublishString());
+            if (lastPublishDt != null && lastPublishDt.Rows.Count > 0)
+            {
+                return lastPublishDt.Rows[0][0].ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        //Function for reading and returning a live table
+        public DataTable LiveTableRead(string tablename)
+        {
+            string lastPublishDate = Convert.ToDateTime(LastPublishDate()).ToString("yyyy-MM-dd hh:mm:ss");
+            string query = String.Format("select * from {0} where ModifiedDate > '{1}'", tablename, lastPublishDate);
+            DataTable liveDt = Read(query, LiveString());
+            return liveDt;
+        }
+
+        //Function for returning the publish datatable
+        public DataTable PublishTableRead(string tablename)
+        {
+            string query = String.Format("select * from {0}", tablename);
+            DataTable publishDt = Read(query, PublishString());
+            return publishDt;
+        }
+
+        //Function for returning the name of the person who modified the details using his userId
+        public string ModifiedBy(string usrId)
+        {
+            string query = String.Format("select Fullname from users where UserID = {0}", usrId);
+            DataTable modifiedBy = Read(query, PublishString());
+            return modifiedBy.Rows[0][0].ToString();
+        }
+
+        //Comparing the publish and live table and writing the results in the file
+        public void Comparison(string tablename, string column_identifiers)
+        {
+
+            //live datatable
+            DataTable liveDt = LiveTableRead(tablename);
+
+            //publish datatable
+            DataTable publishDt = PublishTableRead(tablename);
+
+            //Check if the publish table is empty or not
+            if (liveDt != null && liveDt.Rows.Count > 0)
+            {
+                //Primary Key column name
+                string primaryKey = liveDt.Columns[0].ColumnName;
+
+                //Iterating over every row in liveDt
+                foreach (DataRow lrow in liveDt.Rows)
+                {
+                    //Getting the name of the person who modified the details using the id
+                    string name = ModifiedBy(lrow["ModifiedById"].ToString());
+
+                    //Selecting the row from publishDt where its primary key column value is equal to that in publishDt
+                    DataRow prow = publishDt.Select(String.Format("{0} = {1}", primaryKey, lrow[primaryKey])).FirstOrDefault();
+
+                    //If that row is not null
+                        if(prow != null) 
+                        {
+                            
+                        //Iterate over every column in the liveDt
+                            foreach (DataColumn lcolumn in liveDt.Columns)
+                            {
+                                // Test case 1: Existing column modified
+                                //If the publishDt contains a particular column present in the liveDt
+                                if (publishDt.Columns.Contains(lcolumn.ColumnName))
+                                {
+                                    //Check whether the publishDt column value is equal to the liveDt column value or not
+                                    if (!Equals(prow[lcolumn.ColumnName].ToString(), lrow[lcolumn.ColumnName].ToString()))
+                                    {
+                                    //Ignore the column values which are present in ignoredcolumns list
+                                    if (!ignoredColumns.Contains(lcolumn.ColumnName))
+                                    {
+                                        //Create a new object for the class Changes and add it to the list
+                                            list.Add
+                                                (new Changes
+                                                (tablename,
+                                                (lcolumn.ColumnName != null) ? lcolumn.ColumnName : "NA",
+                                                (name != null) ? name : "NA",
+                                                (lrow["ModifiedDate"] != DBNull.Value) ? lrow["ModifiedDate"].ToString() : "NA",
+                                                (prow[lcolumn.ColumnName] != DBNull.Value) ? prow[lcolumn.ColumnName].ToString() : "NA",
+                                                (lrow[lcolumn.ColumnName] != DBNull.Value) ? lrow[lcolumn.ColumnName].ToString() : "NA",
+                                                (prow[column_identifiers] != DBNull.Value) ? prow[column_identifiers].ToString() : "NA"));
+                                        }
+                                    }
+
+                                }
+
+                                //Test case 2: New column added
+                                else
+                                {
+                                    //If the publishDt does not contain a particular column present in liveDt
+                                    if (!publishDt.Columns.Contains(lcolumn.ColumnName))
+                                    {
+                                    //Ignore the column values which are present in ignoredcolumns list
+                                    if (!ignoredColumns.Contains(lcolumn.ColumnName))
+                                    {
+                                            //if the column value is not null
+                                            if (lrow[lcolumn.ColumnName] != DBNull.Value)
+                                            {
+                                            //Create a new object for the class Changes and add it to the list
+                                            list.Add
+                                                (new Changes
+                                                (tablename,
+                                                (lcolumn.ColumnName != null) ? lcolumn.ColumnName : "NA",
+                                                (name != null) ? name : "NA",
+                                                (lrow["ModifiedDate"] != DBNull.Value) ? lrow["ModifiedDate"].ToString() : "NA",
+                                                "NA",
+                                                (lrow[lcolumn.ColumnName] != DBNull.Value) ? lrow[lcolumn.ColumnName].ToString() : "NA",
+                                                (prow[column_identifiers] != DBNull.Value) ? prow[column_identifiers].ToString() : "NA"));
+                                            }
+
+                                        }
+                                    }
+
+                                    //Test case 3: Existing column deleted
+                                    //Iterate over every column in the publishDt
+                                    foreach (DataColumn pcolumn in publishDt.Columns)
+                                    {
+                                    //If the liveDt does not contain that particular column
+                                        if (!liveDt.Columns.Contains(pcolumn.ColumnName))
+                                        {
+                                        //Ignore the column values which are present in ignoredcolumns list
+                                        if (!ignoredColumns.Contains(lcolumn.ColumnName))
+                                        {
+                                            //Create a new object for the list 
+                                                list.Add
+                                                (new Changes
+                                                (tablename,
+                                                (pcolumn.ColumnName != null) ? pcolumn.ColumnName : "NA",
+                                                (name != null) ? name : "NA",
+                                                (lrow["ModifiedDate"] != DBNull.Value) ? lrow["ModifiedDate"].ToString() : "NA",
+                                                (lrow[lcolumn.ColumnName] != DBNull.Value) ? lrow[lcolumn.ColumnName].ToString() : "NA",
+                                                "NA",
+                                                (prow[column_identifiers] != DBNull.Value) ? prow[column_identifiers].ToString() : "NA"));
+                                            }
+                                        }
+                                    }
+
+                                }
+
+                            }
+                        }
+                    else
+                    {
+                        //Test case 4: New row is added
+
+                        list.Add
+                        (new Changes
+                             (tablename,
+                             (primaryKey != null) ? primaryKey : "NA",
+                             (name != null) ? name : "NA",
+                             (lrow["ModifiedDate"] != DBNull.Value) ? lrow["ModifiedDate"].ToString() : "NA",
+                             "NA",
+                             (lrow[primaryKey].ToString() != null) ? lrow[primaryKey].ToString():"NA",
+                             (lrow[column_identifiers].ToString()!=null)? lrow[column_identifiers].ToString():"NA")    
+                        );
+
+                    }
+                }
+
+                //using the StreamWriter to write the details in the file
+                using (StreamWriter sr = File.AppendText(path))
+                {
+                    foreach (var item in list)
+                    {
+                        sr.WriteLine("{0},{1},{2},{3},{4},{5},{6}",
+                            (tablename != null) ? tablename : "NA",
+                            (item.column_name != null) ? item.column_name : "NA",
+                            (item.name != null) ? item.name : "NA",
+                            (item.mDate != null) ? item.mDate : "NA",
+                            (item.old_data != null) ? item.old_data : "NA",
+                            (item.new_data != null) ? item.new_data : "NA",
+                            (item.row_data != null) ? item.row_data : "NA");
+                    }
+                }
+            }
+
+            //Clear the list
+            list.Clear();
+        }
+        //Test case 5: A row is deleted
+        public void RowComparison(string tablename, string columnIdentifier)
+        {
+
+            //live datatable
+            string query1 = String.Format("select * from {0}", tablename);
+            DataTable liveDt = Read(query1, LiveString());
+
+            //Primary Key
+            string pkey = liveDt.Columns[0].ColumnName;
+
+            //Resultant DataTable
+            DataTable dt = new DataTable();
+
+            //publish datatable
+            string query = String.Format("select * from {0}", tablename);
+            DataTable publishDt = Read(query, PublishString());
+
+            foreach (DataRow prow in publishDt.Rows)
+            {
+                DataRow[] deleted = liveDt.Select(string.Format("{0} = {1}", pkey, prow[pkey].ToString()));
+                if (deleted.Count() == 0)
+                {
+                    foreach(DataColumn pcolumn in publishDt.Columns)
+                    {
+                        //Ignore the column values which are present in ignoredcolumns list
+                        if (!ignoredColumns.Contains(pcolumn.ColumnName))
+                        {
+                            //Create a new object and add it to the list
+                            list.Add
+                                (new Changes
+                                (tablename,
+                                 (pcolumn.ColumnName != null) ? pcolumn.ColumnName : "NA",
+                                 "NA",
+                                 "NA",
+                                 (prow[pcolumn.ColumnName].ToString() != null) ? prow[pcolumn.ColumnName].ToString() : "NA",
+                                 "NA",
+                                 (prow[columnIdentifier].ToString() != null) ? prow[columnIdentifier].ToString() : "NA"
+                                ));
+                        }
+                    }
+                }
+            }
+            //Using the streamwriter write the contents into the file
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                foreach (var item in list)
+                {
+                    sr.WriteLine("{0},{1},{2},{3},{4},{5},{6}",
+                        (tablename != null) ? tablename : "NA",
+                        (item.column_name != null) ? item.column_name : "NA",
+                        (item.name != null) ? item.name : "NA",
+                        (item.mDate != null) ? item.mDate : "NA",
+                        (item.old_data != null) ? item.old_data : "NA",
+                        (item.new_data != null) ? item.new_data : "NA",
+                        (item.row_data != null) ? item.row_data : "NA");
+                }
+            }
+            //Clear the list
+            list.Clear();
+        }
+
+        //Function for deleting the file if it already exists
+        public void FileDelete()
+        {
+            //Deleting the file for a given path
+            if (File.Exists(path))
+                File.Delete(path);
+        }
+        
+    }
+}
---
+++ Code/Code/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConnection db = new DBConnection();
+
+            //List containing table names and column identifiers
+
+            List<KeyValuePair<string, string>> pairs = new List<KeyValuePair<string, string>>();
+            //pairs.Add(new KeyValuePair<string, string>("acccabinets", "CSubTID"));
+            pairs.Add(new KeyValuePair<string, string>("accessories", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("accessoryassemblyshuttertype", "AID"));
+            pairs.Add(new KeyValuePair<string, string>("acchardwares", "HID"));
+            pairs.Add(new KeyValuePair<string, string>("accpanels", "PID"));
+            pairs.Add(new KeyValuePair<string, string>("addonaccessories", "MainAID"));
+            pairs.Add(new KeyValuePair<string, string>("ambiencecategories", "Category"));
+            pairs.Add(new KeyValuePair<string, string>("ambienceitems", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("ambiencerooms", "RID"));
+            pairs.Add(new KeyValuePair<string, string>("appliancecategories", "Category"));
+            pairs.Add(new KeyValuePair<string, string>("applianceitems", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("appliancerooms", "RID"));
+            pairs.Add(new KeyValuePair<string, string>("cabinetconstructiontypeassemblies", "Component"));
+            pairs.Add(new KeyValuePair<string, string>("cabinetconstructiontypehardwares", "HID"));
+            pairs.Add(new KeyValuePair<string, string>("cabinetconstructiontypes", "TypeName"));
+            pairs.Add(new KeyValuePair<string, string>("cabinetfinishtypes", "FTID"));
+            pairs.Add(new KeyValuePair<string, string>("cabinetshuttertypes", "SCID"));
+            pairs.Add(new KeyValuePair<string, string>("cabinetsubtypes", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("cabinetsubtypesizes", "CSubTID"));
+            pairs.Add(new KeyValuePair<string, string>("cabinettypes", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("cities", "CityName"));
+            pairs.Add(new KeyValuePair<string, string>("citycores", "CMRID"));
+            pairs.Add(new KeyValuePair<string, string>("cityglasses", "GID"));
+            pairs.Add(new KeyValuePair<string, string>("citymodules", "CSubTID"));
+            pairs.Add(new KeyValuePair<string, string>("cityshades", "FTSSID"));
+            pairs.Add(new KeyValuePair<string, string>("corematerialfinishes", "FTID"));
+            pairs.Add(new KeyValuePair<string, string>("corematerialrates", "Color"));
+            pairs.Add(new KeyValuePair<string, string>("corematerials", "CMName"));
+            pairs.Add(new KeyValuePair<string, string>("corematerialshuttertypes", "CMID"));
+            pairs.Add(new KeyValuePair<string, string>("designcorethickness", "DID"));
+            pairs.Add(new KeyValuePair<string, string>("designfamilies", "FTID"));
+            pairs.Add(new KeyValuePair<string, string>("designrooms", "RID"));
+            pairs.Add(new KeyValuePair<string, string>("designs", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("designshuttertypes", "RFT"));
+            pairs.Add(new KeyValuePair<string, string>("designvariants", "FTSID"));
+            pairs.Add(new KeyValuePair<string, string>("edgebandings", "EBName"));
+            pairs.Add(new KeyValuePair<string, string>("finishtypes", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("finishtypeseries", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("finishtypeseriesshades", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("glasscategories", "Category"));
+            pairs.Add(new KeyValuePair<string, string>("glasses", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("glassfamilies", "ftid"));
+            pairs.Add(new KeyValuePair<string, string>("glassproducts", "PRID"));
+            pairs.Add(new KeyValuePair<string, string>("handlecategories", "HCName"));
+            pairs.Add(new KeyValuePair<string, string>("handlecategoryhandles", "HandleID"));
+            pairs.Add(new KeyValuePair<string, string>("handlecategoryproducts", "PRID"));
+            pairs.Add(new KeyValuePair<string, string>("handlepositions", "Description"));
+            pairs.Add(new KeyValuePair<string, string>("handles", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("hardwares", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("legproducts", "PRID"));
+            pairs.Add(new KeyValuePair<string, string>("legrooms", "RID"));
+            pairs.Add(new KeyValuePair<string, string>("legs", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("panelcoreselection", "PanelColour"));
+            pairs.Add(new KeyValuePair<string, string>("panels", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("productcabinets", "PRID"));
+            pairs.Add(new KeyValuePair<string, string>("productcores", "CMID"));
+            pairs.Add(new KeyValuePair<string, string>("products", "Name"));
+            //pairs.Add(new KeyValuePair<string, string>("publishpoints", "PPName"));
+            pairs.Add(new KeyValuePair<string, string>("reports", "Reportname"));
+            pairs.Add(new KeyValuePair<string, string>("reportsunused", "Reportname"));
+            pairs.Add(new KeyValuePair<string, string>("roomcabinets", "CTID"));
+            pairs.Add(new KeyValuePair<string, string>("rooms", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("shadeedgebandmap", "EBID"));
+            pairs.Add(new KeyValuePair<string, string>("shuttercombinations", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("shutterhandlepositions", "HPID"));
+            pairs.Add(new KeyValuePair<string, string>("shutterrates", "Rate"));
+            pairs.Add(new KeyValuePair<string, string>("shuttersubtypedims", "Description"));
+            pairs.Add(new KeyValuePair<string, string>("shuttersubtypehardware", "HID"));
+            pairs.Add(new KeyValuePair<string, string>("shuttersubtypes", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("shuttertypes", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("stdcabinetaccessory", "MainAid"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobeaccessories", "Name"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobeaccessorycabinets", "CSubTID"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobeaccessorypanels", "PanelName"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobeacchardwares", "HID"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobeinternalsetaccessories", "AID"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobeinternalsetassemblies", "Component"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobeinternalsetcabinets", "CSubTID"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobeinternalsethardware", "HID"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobeinternalsets", "WISName"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobemultiassemblies", "Description"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobemulticabinets", "CsubTID"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobemultihardware", "HID"));
+            pairs.Add(new KeyValuePair<string, string>("wardrobemultishutters", "WMSName"));
+
+            db.FileDelete();
+
+            foreach (var element in pairs)
+             {
+                 Console.WriteLine("For the table {0}, the generated report is", element.Key);
+                 db.Comparison(element.Key, element.Value);
+                 db.RowComparison(element.Key, element.Value);
+             }
+            //db.rowComparison("cabinetsubtypesizes", "CSubTId");
+            Environment.Exit(0);
+        }
+    }
+}
